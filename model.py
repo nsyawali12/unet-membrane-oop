28e5ceb51ce315fe76404e0c41b04cd28cdda84f
nsyawali12 <nsyawali12@gmail.com>
2021-07-26 01:32:53 +0700

    model build

---
+++ model.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
